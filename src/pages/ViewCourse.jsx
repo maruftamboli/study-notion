deb2331a6f0dde31fbec7cd96fd7d777074b9432
Maruf Tamboli <maruftamboli1@gmail.com>
2024-06-14 11:19:43 +0530

    first commit

---
+++ src/pages/ViewCourse.jsx
@@ -0,0 +1,62 @@
+import React from "react"
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Outlet, useParams } from "react-router-dom"
+
+import CourseReviewModal from "../components/core/ViewCourse/CourseReviewModal"
+import VideoDetailsSidebar from "../components/core/ViewCourse/VideoDetailsSidebar"
+import { getFullDetailsOfCourse } from "../services/operations/courseDetailsAPI"
+import {
+  setCompletedLectures,
+  setCourseSectionData,
+  setEntireCourseData,
+  setTotalNoOfLectures,
+} from "../slices/viewCourseSlice"
+
+export default function ViewCourse() {
+  const { courseId } = useParams()
+  const { token } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  const [reviewModal, setReviewModal] = useState(false)
+
+  useEffect(() => {
+    ;(async () => {
+      const courseData = await getFullDetailsOfCourse(courseId, token);
+      dispatch(setCourseSectionData(courseData.courseDetails.courseContent));
+      dispatch(setEntireCourseData(courseData.courseDetails));
+      dispatch(setCompletedLectures(courseData.completedVideos));
+      
+      let lectures = 0;
+      if (courseData?.courseDetails?.courseContent) {
+        courseData.courseDetails.courseContent.forEach((sec) => {
+          // Ensure sec.subSection is an array before accessing its length
+          if (Array.isArray(sec.subSection)) {
+            lectures += sec.subSection.length;
+          }
+        });
+      }
+      dispatch(setTotalNoOfLectures(lectures));
+    })();
+  
+    // Dummy statement to satisfy the linter
+    console.log("useEffect executed");
+  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
+  
+
+  return (
+    <>
+      <div className="relative flex min-h-[calc(100vh-3.5rem)]">
+        <VideoDetailsSidebar setReviewModal={setReviewModal} />
+        <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
+          <div className="mx-6">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+      {reviewModal && <CourseReviewModal setReviewModal={setReviewModal} />}
+    </>
+  )
+}
